e28556b4ae841c1bac65b3667aafadd83f58d6d0
CHRISTOS PAPACHARALAMPOUS <40805395+Chrispapah@users.noreply.github.com>
2025-09-18 14:50:44 +0300

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 console.log("NP worker loaded");
 
-chorme.runtime.onMessage.addlistener (m=>{if (m.type == "RUN_NOW") console.log("RUN_NOW received");});
+chrome.runtime.onMessage.addlistener (m=>{if (m.type == "RUN_NOW") console.log("RUN_NOW received");});
 
 chrome.alarms.onAlarm.addlistener (a=> console.log("alarm",a.name));
